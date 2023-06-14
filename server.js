89f3da9f87970d6117807548369977eb92be0db7
vibhu0209 <vibhu0209@gmail.com>
2023-06-14 12:34:11 +0530

    No Update

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
         io.to(roomId).emit("user-connected",userId)
-        socket.on("message", (message) => {
+        socket.on("message",(message) => {
             io.to(roomId).emit("createMessage", message, userName);
         });
     });
