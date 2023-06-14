8c50a8be0e8f1347026a79dc270bd6297acee727
vibhu0209 <vibhu0209@gmail.com>
2023-06-14 12:50:06 +0530

    buttons working

--- public/script.js
+++ public/script.js
@@ -52,6 +52,7 @@ function addVideoStream(video,stream){
     })
 }
 
+
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
@@ -78,7 +79,39 @@ $(function () {
         }
     })
 
+    $("#stop_video").click(function(){
+        const enabled = myStream.getVideoTracks()[0].enabled
+        if(enabled){
+            myStream.getVideoTracks()[0].enabled == false
+            html = `<i class="fas fa-video-slash"></i>`
+            $("#stop_video").toggleClass("background_red")
+            $("#stop_video").html(html)
+        }
+        else{
+            myStream.getVideoTracks()[0].enabled == true
+            html = `<i class="fas fa-microphone"></i>`
+            $("#stop_video").toggleClass("background_red")
+            $("#stop_video").html(html)
+        }
+    })
+    
+    $("#mute_button").click(function(){
+        const enabled = myStream.getAudioTracks()[0].enabled
+        if(enabled){
+            myStream.getAudioTracks()[0].enabled == false
+            html = `<i class="fas fa-microphone-slash"></i>`
+            $("#mute_button").toggleClass("background_red")
+            $("#mute_button").html(html)
+        }
+        else{
+            myStream.getAudioTracks()[0].enabled == true
+            html = `<i class="fas fa-microphone"></i>`
+            $("#mute_button").toggleClass("background_red")
+            $("#mute_button").html(html)
+        }
+    })
 })
+
 
 peer.on("open", (id) => {
     socket.emit("join-room", ROOM_ID, id, user);
